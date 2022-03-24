972b13118b92c82159d71275dbb11cc1d1555630
malexes <maleksander@swps.edu.pl>
2022-03-24 17:50:26 +0100

    my lib

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="abc",
+    version='0.1',
+    description='this is library',
+    url='',
+    author='i am author',
+    author_email='my email is private info',
+    license='unlicense',
+    packages=['my_lib'],
+    zip_safe=False
+)
